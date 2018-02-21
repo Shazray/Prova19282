e598aec9c42548060b4ad7c6f2af3b70a172a730
Shazray <virgilio.lofrano10@gmail.com>
2018-02-21 11:50:18 +0100

    pulito l'app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,12 @@ import { AfterViewComponent } from './after-view/after-view.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = "Hello Class";
-  items: Character[] = [];
-  value: string;
-  currentCharacter: Character;
-  @ViewChild(AfterViewComponent) viewChild: AfterViewComponent;
 
 
-  constructor(private listService: ListService) {
+  constructor() {
 
-    this.items = listService.getCharactersList();
   }
 
-  showElementChanged(Character: Character)
-  {
-    
-    //this.currentCharacter = Character;
-    
-  }
-
-  showValue() {
-
-    alert("valore: " + this.value);
-  }
-
-ngAfterViewInit(){
-  this.value = this.viewChild.valueAfterView;
-
-
-}
-
-ngAfterViewChecked(){
-
-  this.value = this.viewChild.valueAfterView;
-}
 
 
 }
